16eab0fbd725be3550bdf15275d4c13eea163497
Abu Talha <rogdex24@gmail.com>
2024-03-29 11:20:30 +0530

    update correct KMS key alias in bash script
    changed the key name accordingly to what is set in the cloudformation stack when the key alias is set.

--- ebs/Bash/enable-ebs-encryption-for-account.sh
+++ ebs/Bash/enable-ebs-encryption-for-account.sh
@@ -5,7 +5,7 @@ aws ec2 enable-ebs-encryption-by-default
 
 aws ec2 get-ebs-default-kms-key-id
 
-EBS_CMK_ARN=$(aws kms describe-key --key-id 'alias/EC2EncryptionAtRestCMKAlias' |jq --raw-output '.KeyMetadata.Arn')
+EBS_CMK_ARN=$(aws kms describe-key --key-id 'alias/EC2EncryptionAtRestKeyAlias' |jq --raw-output '.KeyMetadata.Arn')
 
 aws ec2 modify-ebs-default-kms-key-id --kms-key-id "$EBS_CMK_ARN"
 
